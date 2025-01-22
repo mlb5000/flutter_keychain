0410da24f1fec116facd26b58e9900144f44dd7c
Matthew Baker <github@bakermatt.com>
2025-01-22 06:56:46 -0500

    Updates

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_keychain
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterKeychainPlugin.register(with: registry.registrar(forPlugin: "FlutterKeychainPlugin"))
+}
